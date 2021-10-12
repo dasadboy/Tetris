75abad366dc86fad4f3d37a0cbbbe561dc257d08
dasadboy <akd741@gmail.com>
2021-10-12 16:02:38 -0400

    moved 'Game' class to 'game.h'

--- game.cpp
+++ game.cpp
@@ -1,5 +1,5 @@
 class Game {
-    bool board[400] = {false}; // single dimension array representing 40x10 board;
+    bool board[400] = { false }; // single dimension array representing 40x10 board;
     int maxHeight = 0;
     // void Game() {
     //     return;
