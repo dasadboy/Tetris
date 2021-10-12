e8b6e6b1b991cf54073a04f9d3fed891e42b1682
dasadboy <akd741@gmail.com>
2021-10-12 16:40:54 -0400

    Added board class and method declarations

--- game.h
+++ game.h
@@ -1,8 +1,17 @@
+class Board {
+    bool board[400];
+
+    bool get(bool rowCol);
+
+    bool get(bool row, bool col);
+
+    bool set(bool rowCol, bool val);
+
+    bool set(bool row, bool col, bool val);
+};
 
 class Game {
     bool board[400] = { false }; // single dimension array representing 40x10 board;
     int maxHeight = 0;
-    // void Game() {
-    //     return;
-    // }
+    Game();
 };
