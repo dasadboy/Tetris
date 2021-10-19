5fc168046171c94cb2cbc79c4516c3204a863af8
dasadboy <akd741@gmail.com>
2021-10-18 22:08:41 -0400

    removed functions to be handled by board

--- pieces.cpp
+++ pieces.cpp
@@ -1,9 +1,7 @@
 #include <algorithm>
 #include "pieces.h"
-#include "game.h"
 
 using namespace std;
-
 
 Square::Square() {
     pos = 205;
@@ -12,46 +10,6 @@ Square::Square() {
 void Square::rotate() {
     // rotating results in the same shape
     return;
-}
-
-void Square::down() {
-    if (pos >= 0 || board[pos - 10] || board[pos - 9]) {
-        set();
-        return;
-    }
-    pos -= 10;
-    // move up one row if too low
-}
-
-void Square::left() {
-    if (pos % 10 > 0)
-        pos -= 1;
-}
-
-void Square::right() {
-    if (pos % 10 < 8)
-        pos += 1;
-}
-
-void Square::drop() {
-    // find distance, move, and place
-    int n = pos - 10;
-    while (n >= 0 || !board[n] && !board[n+1]) {
-        n -= 10;
-    }
-    pos = n + 10;
-    set();
-    return;
-}
-
-void Square::set() {
-    // set board
-    board[pos] = true;
-    board[pos + 1] = true;
-    board[pos + 10] = true;
-    board[pos + 11] = true;
-    // draw
-    delete this;
 }
 
 
@@ -78,63 +36,6 @@ void TBlock::rotate() {
     }
 }
 
-void TBlock::down() {
-    blocks[0] -= 10;
-    blocks[1] -= 10;
-    blocks[2] -= 10;
-    blocks[3] -= 10;
-    if (blocks[1] > 0 || blocks[2] > 0 || blocks[3] > 0 || board[blocks[1]] || board[blocks[2]] || board[blocks[3]]) {
-        blocks[0] += 10;
-        blocks[1] += 10;
-        blocks[2] += 10;
-        blocks[3] += 10;
-        set();
-    }
-}
-
-void TBlock::left() {
-    if (blocks[1] % 10 > 0 || blocks[2] % 10 > 0 || blocks[3] % 10 > 0) {
-        blocks[0] -= 1;
-        blocks[1] -= 1;
-        blocks[2] -= 1;
-        blocks[3] -= 1;
-    }
-}
-
-void TBlock::right() {
-    if (blocks[1] % 10 < 8 || blocks[2] % 10 < 8 || blocks[3] % 10 < 8) {
-        blocks[0] += 1;
-        blocks[1] += 1;
-        blocks[2] += 1;
-        blocks[3] += 1;
-    }
-}
-
-void TBlock::drop() {
-    // find distance move and place
-    int dist = 0;
-    while (blocks[1] - dist - 10 >= 0 || blocks[2] - dist - 10 >= 0
-        || blocks[3] - dist - 10 >= 0 || board[blocks[0] - dist - 10]
-        || board[blocks[1] - dist - 10] || board[blocks[2] - dist - 10]
-        || board[blocks[3] - dist - 10]) {
-        dist += 10;
-    }
-    for (int i = 0; i < 4; ++i) {
-        blocks[i] -= dist;
-    }
-    set();
-    return;
-}
-
-void TBlock::set() {
-    board[blocks[0]] = true;
-    board[blocks[1]] = true;
-    board[blocks[2]] = true;
-    board[blocks[3]] = true;
-    // draw
-    delete this;
-}
-
 
 // LBlockL
 
@@ -154,45 +55,6 @@ void LBlockL::rotate() {
     blocks[2] = blocks[0] + stateChange[offset++];
     blocks[3] = blocks[0] + stateChange[offset++];
     offset %= 12;
-    while (board[blocks[0]] || board[blocks[1]] || board[blocks[2]] || board[blocks[3]]) {
-        blocks[0] += 10;
-        blocks[1] += 10;
-        blocks[2] += 10;
-        blocks[3] += 10;
-    }
-}
-
-void LBlockL::down() {
-    if (board[blocks[1]] - 10 > 0 || board[blocks[3]] - 10 > 0 || board[blocks[0]] || board[blocks[1]] || board[blocks[2]] || board[blocks[3]]) {
-        set();
-        return;
-    }
-    blocks[0] -= 10;
-    blocks[1] -= 10;
-    blocks[2] -= 10;
-    blocks[3] -= 10;
-}
-
-void LBlockL::drop() {
-    // find distance, move, and place
-    int dist = 0;
-    while (blocks[1] - dist - 10 >= 0 || blocks[3] - dist - 10 >= 0 || board[blocks[1] - dist - 10] || board[blocks[2] - dist - 10] || board[blocks[3] - dist - 10]) {
-        dist += 10;
-    }
-    for (int i = 0; i < 4; ++i) {
-        blocks[i] -= dist;
-    }
-    set();
-    return;
-}
-
-void LBlockL::set() {
-    board[blocks[0]] = true;
-    board[blocks[1]] = true;
-    board[blocks[2]] = true;
-    board[blocks[3]] = true;
-    // draw
-    delete this;
 }
 
 
@@ -214,52 +76,5 @@ void LBlockR::rotate() {
     blocks[2] = blocks[0] + stateChange[offset++];
     blocks[3] = blocks[0] + stateChange[offset++];
     ++offset;
-    offset %= 4;
-    while (blocks[1] < 0 || blocks[3] < 0 || board[blocks[0]] || board[blocks[1]] || board[blocks[2]] || board[blocks[3]]) {
-        blocks[0] += 10;
-        blocks[1] += 10;
-        blocks[2] += 10;
-        blocks[3] += 10;
-    }
-}
-
-void LBlockR::down() {
-    blocks[0] -= 10;
-    blocks[1] -= 10;
-    blocks[2] -= 10;
-    blocks[3] -= 10;
-    if (board[blocks[0]] || board[blocks[1]] || board[blocks[2]] || board[blocks[3]]) {
-        blocks[0] += 10;
-        blocks[1] += 10;
-        blocks[2] += 10;
-        blocks[3] += 10;
-        set();
-    }
-}
-
-void LBlockR::drop() {
-    // find distance, move, and place
-    int dist = 0;
-    while (blocks[1] - dist - 10 >= 0 || blocks[2] - dist - 10 >= 0 || blocks[3] - dist - 10 >= 0 || board[blocks[1] - dist - 10] || board[blocks[2] - dist - 10] || board[blocks[3] - dist - 10]) {
-        dist += 10;
-    }
-    for (int i = 0; i < 4; ++i) {
-        blocks[i] -= dist;
-    }
-    set();
-    return;
-}
-
-void LBlockR::draw() {
-    // todo
-    return;
-}
-
-void LBlockR::set() {
-    board[blocks[0]] = true;
-    board[blocks[1]] = true;
-    board[blocks[2]] = true;
-    board[blocks[3]] = true;
-    draw();
-    delete this;
+    offset %= 12;
 }
--- pieces.h
+++ pieces.h
@@ -2,52 +2,29 @@
 #define Piece_Class_H
 
 #include <algorithm>
-#include "board.h"
 
-class Piece {
-public:
-    // playfield is 10 wide 40 tall.
-    // cell called using y * 10 + x
-    static Board board;
-};
+class Piece {};
 
 class Square : public Piece {
+public:
     int pos;
 
     Square();
 
     void rotate();
-
-    void down();
-
-    void left();
-
-    void right();
-
-    void drop();
-
-    void set();
 };
 
 class TBlock : public Piece {
+public:
     int blocks[4];
 
     TBlock();
 
     void rotate();
-
-    void down();
-
-    void left();
-
-    void right();
-
-    void drop();
-
-    void set();
 };
 
 class LBlockL : public Piece {
+public:
     int blocks[4];
     int offset;
     static const int stateChange[12]; // position of pieces relative to centre piece
@@ -55,17 +32,10 @@ class LBlockL : public Piece {
     LBlockL();
 
     void rotate();
-
-    void down();
-
-
-
-    void drop();
-
-    void set();
 };
 
 class LBlockR : public Piece {
+public:
     int blocks[4];
     int offset;
     static const int stateChange[12]; // position of pieces relative to centre piece block[0]
@@ -73,33 +43,13 @@ class LBlockR : public Piece {
     LBlockR();
 
     void rotate();
-
-    void down();
-
-    void drop();
-
-    void draw();
-
-    void set();
 };
 
 // TODO
-//class StraightBlock : public Piece {
-//    int blocks[4];
-//    int state = 0;
-//    int stateChange[4][4];
-//    
-//    StraightBlock();
-//    
-//    void rotate();
-//
-//    void down();
-//    
-//    void drop();
-//
-//    void draw();
-//
-//    void set();
-//};
+class Straight : public Piece {};
+
+class ZBlock : public Piece {};
+
+class SBlock : public Piece {};
 
 #endif
