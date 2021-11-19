5d2ee186488004d130edccaadfbb7e820cd0693f
dasadboy <akd741@gmail.com>
2021-11-19 17:55:24 -0500

    BoardCell class and incorporated Block class

--- includes/board.h
+++ includes/board.h
@@ -1,37 +1,54 @@
 #pragma once
 
+#include "blocks.h"
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <string>
-#include "blocks.h"
 
 struct indices {
     int i, j;
 };
 
+struct BoardCell {
+    Block& block;
+    bool isOccupied;
+    BoardCell(Block& b, bool state = false) : block(b) {
+        this->isOccupied = state;
+    }
+    void setOccupied() {
+        this->isOccupied = true;
+    }
+    void operator=(BoardCell& src) {
+        Block newBlock = src.block;
+        sf::Vector2f currPosition = this->block.getPos();
+        newBlock.setPos(currPosition);
+        this->isOccupied = src.isOccupied;
+    }
+    void reset(int row, int col) {
+        bool isOccupied = false;
+        block = Block(row, col);
+    }
+};
 
 class Board {
     // playfield is 10 wide 40 tall.
     // cell called using y * 10 + x
-    std::vector<int> board;
+    std::vector<BoardCell> board;
     std::vector<int> blocksPerRow;
     int currentHeight;
 public:
 
     Board();
 
-    int& operator[](int rowCol);
+    BoardCell& operator[](int rowCol);
 
-    int& operator[](indices rowCol); // called with board({row, col}) or given object 'obj' of type indices, board(obj)
+    BoardCell& operator[](indices rowCol); // called with board({row, col}) or given object 'obj' of type indices, board(obj)
 
     bool checkPositionLegal(int row, int col);
 
     void removeFilledRows(int rowStart); // remove filled rows
 
-    void setPiece(std::vector<int> rows, std::vector<int> cols, int color);
-
-    void drawBlock(int row, int col);
+    void setPiece(std::vector<int> rows, std::vector<int> cols, std::vector<Block> blocks);
 
     void draw();
 };
