dc2ec501d0adf023f7d0cbc2dece5d62baea964e
dasadboy <akd741@gmail.com>
2021-11-21 22:20:49 -0500

    changed variable names and definitions in COLOR namespace

--- includes/constants.h
+++ includes/constants.h
@@ -28,22 +28,20 @@ namespace PIECES {
 }
 
 namespace COLOR { 
-
-	const int COLOR_EMPTY = 0;
 	
-	const int COLOR_SQUARE = 1;
+	const sf::Color COLOR_OPIECE = sf::Color::Yellow;
 
-	const int COLOR_LBLOCKL = 2;
+	const sf::Color COLOR_JPIECE = sf::Color::Blue;
 
-	const int COLOR_LBLOCKR = 3;
+	const sf::Color COLOR_LPIECE = sf::Color::Red + sf::Color::Yellow; // Orange doesn't exist
 
-	const int COLOR_TBLOCK = 4;
+	const sf::Color COLOR_TPIECE = sf::Color::Red + sf::Color::Blue; // Purple doesn't exist
 
-	const int COLOR_ZBLOCK = 5;
+	const sf::Color COLOR_ZPIECE = sf::Color::Red;
 
-	const int COLOR_SBLOCK = 6;
+	const sf::Color COLOR_SPIECE = sf::Color::Green;
 
-	const int COLOR_STRAIGHT = 7;
+	const sf::Color COLOR_IPIECE = sf::Color::Cyan;
 }
 
 namespace BOARD {
